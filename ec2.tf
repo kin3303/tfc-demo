b91ae152ec096d0eeb59e925a3b407dec3cec4e4
kin3303 <48729027+kin3303@users.noreply.github.com>
2021-12-29 19:04:24 +0900

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
 resource "aws_instance" "example" {
   ami           = "ami-f293459c"
   instance_type = "t2.micro"
