a443ec25ec89b42d0c3b43c329890fc66356ea86
kin3303 <48729027+kin3303@users.noreply.github.com>
2021-12-30 14:06:00 +0900

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "instance_ip_addr" {
-    value = aws_instance.server.private_ip
+    value = aws_instance.example.private_ip
     description = "The private IP address of the main server instance."
     sensitive   = true
 }
